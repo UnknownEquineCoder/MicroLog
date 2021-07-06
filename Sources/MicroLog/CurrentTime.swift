b650ee74c7856114e37f5856472c12fa1037c568
UnknownEquineCoder <antoninorossidev@gmail.com>
2021-07-06 14:39:16 +0200

    Fixed bug with timestamp

--- Sources/MicroLog/CurrentTime.swift
+++ Sources/MicroLog/CurrentTime.swift
@@ -3,7 +3,7 @@ import Foundation
 public extension Date {
     static var currentTime: String {
         let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-mm-dd HH:mm:ss.SSSS"
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss.SSSS"
         
         return formatter.string(from: .init())
     }
