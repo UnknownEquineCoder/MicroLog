4a3312c1f67db29a4c2cc959ae6686be9e7d064e
UnknownEquineCoder <antoninorossidev@gmail.com>
2021-07-06 21:05:43 +0200

    Added documentation to Date extension.

--- Sources/MicroLog/CurrentTime.swift
+++ Sources/MicroLog/CurrentTime.swift
@@ -1,6 +1,9 @@
 import Foundation
 
 public extension Date {
+    /**
+     Returns the current date and time (with milliseconds) as a String.
+     */
     static var currentTime: String {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd HH:mm:ss.SSSS"
